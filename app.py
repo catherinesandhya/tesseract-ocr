9c4b4271e46125d520c5ead8c57e95230a6c3f05
catherinesandhya <92034486+catherinesandhya@users.noreply.github.com>
2021-10-08 14:33:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ pytesseract.pytesseract.tesseract_cmd='/app/.apt/usr/bin/tesseract'  #for heroku
 st.set_option('deprecation.showfileUploaderEncoding',False) #Ignores the warning
 st.title("OCR - Optical Character Recognition")
 st.text('Upload the image')  #like write
-uploaded_file=st.sidebar.file_uploader('Choose an image',type=["jpg","jpeg","png"])  #create a file uploader and can upload the image with 3 extensions given
+uploaded_file=st.sidebar.file_uploader('Choose an image',type=["jpg","jpeg","png"])  #create   a file uploader and can upload the image with 3 extensions given
 if uploaded_file is not None:   #Only if a file is uploaded, perform the below
   img=Image.open(uploaded_file)  #opens uploaded file
   st.image(img,caption='Uploaded Image',use_column_width=True)  #display the uploaded image with original width and height
